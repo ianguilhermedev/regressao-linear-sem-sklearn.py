8b2c12c355ba9a8a3c9c51b62ce2761496556cc7
ianguilhermedev <ian.guilherme.dev@gmail.com>
2026-05-28 17:39:35 -0300

    regressao-linear-sem-sklearn.py

--- regressao-linear-sem-sklearn.py
+++ regressao-linear-sem-sklearn.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+
 def main():
     x = [3.5, 3.69, 3.44, 3.43, 4.34, 4.42, 2.37]  # peso em 1000s
     y = [18, 15, 18, 16, 15, 14, 24] # milhas por galão
@@ -7,46 +8,64 @@ def main():
     w = 0
     b = 0
 
-    for iterações in range(10000): # EPOCH
-        predict = []
-        for feature in x:
-            label = b + feature*w
-            predict.append(label)
+    learning_rate = 0.01
+    epochs = 10000
+    
+    treinar_modelo_regressao_linear(x, y, b, w, epochs, learning_rate)
+
+
+
+def treinar_modelo_regressao_linear(x, y, b, w, epochs, learning_rate):
+    for iteracoes in range(epochs): # EPOCH
+        predicts = calculo_predicts(x, b, w)
 
         #calcula o loss utilizando o mse
-        soma_mse = 0
-        for i in range(len(y)):
-            soma_mse += pow((predict[i] - y[i]), 2)
-        mean_squared_error = soma_mse / len(x)
+        mean_squared_error = calculo_loss(x, y, predicts)
 
         #DERIVADA PARCIAL DE DO PESO 
-        soma_parcial_w = 0
-        for i in range(len(y)):
-            soma_parcial_w += (predict[i] - y[i]) * (2*x[i]) # calculo da parcial de x
-        parcial_w = soma_parcial_w / len(x)
+        parcial_w, parcial_b = gradiente_descendente(x, y, predicts)
 
-        #PARCIAL DO BIAS/INTERCEPT 
-        soma_parcial_b = 0
-        for i in range(len(y)):
-            soma_parcial_b += (predict[i] - y[i]) * 2 # calculo da parcial de x
-        parcial_b = soma_parcial_b / len(x)
+        w, b = atualizando_parametros(w, b ,parcial_w, parcial_b, learning_rate)
+    plot_grafico(x, y, predicts, mean_squared_error)
 
-        w, b = atualizando_parametros(w, b ,parcial_w, parcial_b)
-    plot_grafico(x, y, predict, mean_squared_error)
+def calculo_predicts(x, b, w):
+    list = []
+    for feature in x:
+        predict = b + feature*w
+        list.append(predict)
+    return list
+
+def calculo_loss(x, y, predicts):
+    soma_mse = 0
+    for i in range(len(y)):
+        soma_mse += (predicts[i] - y[i]) ** 2
+
+    return soma_mse / len(x)
         
+def gradiente_descendente(x, y, predicts):
+    soma_parcial_w = 0
+    for i in range(len(y)):
+        soma_parcial_w += (predicts[i] - y[i]) * (2*x[i]) # calculo da parcial de x
+    parcial_w = soma_parcial_w / len(x)
 
-def atualizando_parametros(w, b, parcial_w, parcial_b):
-    learning_rate = 0.01
+    soma_parcial_b = 0
+    for i in range(len(y)):
+        soma_parcial_b += (predicts[i] - y[i]) * 2 # calculo da parcial de x
+    parcial_b = soma_parcial_b / len(x)
+
+    return (parcial_w, parcial_b)
+
+def atualizando_parametros(w, b, parcial_w, parcial_b, learning_rate):
     return(w - learning_rate*parcial_w, b - learning_rate*parcial_b)
 
-def plot_grafico(eixox, eixoy, predict, loss):
+def plot_grafico(eixox, eixoy, predicts, loss):
     plt.plot(eixox, eixoy, 'o')
     plt.grid(True)
     plt.xlabel('Peso em 1000s')
     plt.ylabel('Milhas Por Galão')
     plt.title(f'Peso x Milhas por Galão | Loss: {loss:.3f}')
 
-    plt.plot(eixox, predict)
+    plt.plot(eixox, predicts)
     plt.legend(['Valor Real',
                 'Valor Predictado'])
     plt.show()
