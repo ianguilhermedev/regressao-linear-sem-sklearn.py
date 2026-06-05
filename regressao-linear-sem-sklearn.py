beb9788e0fb64f053c0fbc5dec912eb57376d50c
ianguilhermedev <ian.guilherme.dev@gmail.com>
2026-06-05 20:06:24 -0300

    Update regressao-linear-sem-sklearn.py

--- regressao-linear-sem-sklearn.py
+++ regressao-linear-sem-sklearn.py
@@ -1,74 +1,83 @@
 import matplotlib.pyplot as plt
 
+class LinearRegression:
+    def __init__(self):
+        self.w = 0
+        self.b = 0
+        self.loss = 0
+        self.predicts = []
 
-def main():
-    x = [3.5, 3.69, 3.44, 3.43, 4.34, 4.42, 2.37]  # peso em 1000s
-    y = [18, 15, 18, 16, 15, 14, 24] # milhas por galão
-
-    w = 0
-    b = 0
-
-    learning_rate = 0.01
-    epochs = 10000
+    @property
+    def coef_(self):
+        return self.w
     
-    treinar_modelo_regressao_linear(x, y, b, w, epochs, learning_rate)
+    @property
+    def intercept_(self):
+        return self.b
+    
+    def fit(self, x:object, y:object, epochs=10000, learning_rate=0.01) -> None:
+        for _ in range(epochs):
+            predicts = [] # EPOCH
+            for feature in x:
+                predict = self.b + feature*self.w
+                predicts.append(predict)
 
+            #calcula o loss utilizando o mse
+            soma_mse = 0
+            for i in range(len(y)):
+                soma_mse += (self.predicts[i] - y[i]) ** 2
 
+            self.loss = soma_mse / len(x)
 
-def treinar_modelo_regressao_linear(x, y, b, w, epochs, learning_rate):
-    for iteracoes in range(epochs): # EPOCH
-        predicts = calculo_predicts(x, b, w)
+            #DERIVADA PARCIAL DE DO PESO 
+            soma_parcial_w = 0
+            for i in range(len(y)):
+                soma_parcial_w += (self.predicts[i] - y[i]) * (2*x[i]) # calculo da parcial de x
+            parcial_w = soma_parcial_w / len(x)
 
-        #calcula o loss utilizando o mse
-        mean_squared_error = calculo_loss(x, y, predicts)
+            soma_parcial_b = 0
+            for i in range(len(y)):
+                soma_parcial_b += (self.predicts[i] - y[i]) * 2 # calculo da parcial de x
+            parcial_b = soma_parcial_b / len(x)
 
-        #DERIVADA PARCIAL DE DO PESO 
-        parcial_w, parcial_b = gradiente_descendente(x, y, predicts)
+            self.w, self.b = self.w - learning_rate*parcial_w, self.b - learning_rate*parcial_b
+        self.predicts = predicts
 
-        w, b = atualizando_parametros(w, b ,parcial_w, parcial_b, learning_rate)
-    plot_grafico(x, y, predicts, mean_squared_error)
+    def predict(self, feature_value):
+        final_predicts = []
+        for x in feature_value:
+            predicts = self.b + self.w*x
+            final_predicts.append(predicts)
+        return final_predicts
 
-def calculo_predicts(x, b, w):
-    list = []
-    for feature in x:
-        predict = b + feature*w
-        list.append(predict)
-    return list
+    def plot_grafico(self, x,y):
+        plt.plot(x, y, 'o')
+        plt.grid(True)
+        plt.title(f'weight: {self.coef_} | Intercept: {self.intercept_} | Loss: {self.loss:.2f}')
+        plt.xlabel('Horas Estudadas Por Semana')
+        plt.ylabel('Nota Final')
+        plt.plot(x, self.predicts)   # linha da reta ajustada
+        plt.legend(['Valores Reais', 'Previsão'])
+        plt.show()
 
-def calculo_loss(x, y, predicts):
-    soma_mse = 0
-    for i in range(len(y)):
-        soma_mse += (predicts[i] - y[i]) ** 2
+    
+def main():
+    x = [1,   1,   2,   2,   3,   3,   4,   4,   5,   5,   6,   6,
+     7,   7,   8,   8,   9,   9,  10,  10,  11,  12,  12,  13,  14]
+    # horas de estudo por semana
 
-    return soma_mse / len(x)
-        
-def gradiente_descendente(x, y, predicts):
-    soma_parcial_w = 0
-    for i in range(len(y)):
-        soma_parcial_w += (predicts[i] - y[i]) * (2*x[i]) # calculo da parcial de x
-    parcial_w = soma_parcial_w / len(x)
+    y = [1.5, 2.0, 2.0, 2.5, 2.5, 3.5, 3.0, 4.0, 3.8, 4.5, 4.5, 5.2,
+     5.0, 5.8, 5.5, 6.5, 6.0, 7.0, 6.8, 7.5, 7.2, 8.0, 8.5, 8.5, 9.0]
+    # nota final    
 
-    soma_parcial_b = 0
-    for i in range(len(y)):
-        soma_parcial_b += (predicts[i] - y[i]) * 2 # calculo da parcial de x
-    parcial_b = soma_parcial_b / len(x)
+    model = LinearRegression()
+    model.fit(x, y)
+    predicts = model.predict(x)
+    loss = model.loss
+    print(predicts)
+    print(model.coef_)
+    print(model.intercept_)
 
-    return (parcial_w, parcial_b)
-
-def atualizando_parametros(w, b, parcial_w, parcial_b, learning_rate):
-    return(w - learning_rate*parcial_w, b - learning_rate*parcial_b)
-
-def plot_grafico(eixox, eixoy, predicts, loss):
-    plt.plot(eixox, eixoy, 'o')
-    plt.grid(True)
-    plt.xlabel('Peso em 1000s')
-    plt.ylabel('Milhas Por Galão')
-    plt.title(f'Peso x Milhas por Galão | Loss: {loss:.3f}')
-
-    plt.plot(eixox, predicts)
-    plt.legend(['Valor Real',
-                'Valor Predictado'])
-    plt.show()
+    model.plot_grafico(x, y)
 
 main()
-
